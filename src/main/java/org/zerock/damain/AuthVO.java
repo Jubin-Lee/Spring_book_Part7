611b120c6e18b04410e0e3efbc2c515a8a7b588d
Jubin-Lee <l_heure_bleu@naver.com>
2022-02-25 09:05:22 +0900

    로그아웃 쿠키 삭제까지//

---
+++ src/main/java/org/zerock/damain/AuthVO.java
@@ -0,0 +1,10 @@
+package org.zerock.damain;
+
+import lombok.Data;
+
+@Data
+public class AuthVO {
+
+    private String userid;
+    private String auth;
+}
---
+++ src/main/java/org/zerock/mapper/MemberMapper.java
@@ -0,0 +1,8 @@
+package org.zerock.mapper;
+
+import org.zerock.damain.MemberVO;
+
+public interface MemberMapper {
+
+    public MemberVO read(String userid);
+}
